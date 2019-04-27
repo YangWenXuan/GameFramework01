5c6da7cb5a17a83fec08c18258245fb1fec98b17
YangWenXuan <282090664@qq.com>
2019-04-27 22:42:13 +0800

    加入Game类

---
+++ Assets/Game/Game.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed partial class Game : MonoBehaviour
+{
+    private bool inited = false;
+    public static Game Instance { get; private set; }
+    public bool Inited { get { return inited; } }
+
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Init());
+    }
+
+    IEnumerator Init()
+    {
+        //资源第一级优先初始化，其他初始化放在后面
+        yield return null;
+    }
+}
