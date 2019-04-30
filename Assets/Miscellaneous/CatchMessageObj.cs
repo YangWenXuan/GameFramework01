9674941e2562e287cb9494e582b1037fcf25c6d3
YangWenXuan <282090664@qq.com>
2019-04-30 18:50:17 +0800

    加入安卓与UNITY互相调用的测试

---
+++ Assets/Miscellaneous/CatchMessageObj.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CatchMessageObj : MonoBehaviour
+{
+
+    public Text text01;
+    public Text text02;
+    public Text text03;
+
+    int index1=0;
+    int index2=0;
+    int index3=0;
+
+    public void Method1forShow(string value)
+    {
+        index1++;
+        text01.text=value+index1.ToString();
+    }
+
+
+    public void Method2forPause(string value)
+    {
+        index2++;
+        text02.text=value+index2.ToString();;
+    }
+
+
+    public void Method3forRotate(string value)
+    {
+        index3++;
+        text03.text=value+index3.ToString();
+    }
+
+}
